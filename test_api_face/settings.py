f9edb76e8bd27a91334ccbafca5aaa72c569a810
AliDalla <alidalla9500@gmail.com>
2024-10-28 20:49:44 +0300

    the 2 commit

--- test_api_face/settings.py
+++ test_api_face/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# قراءة SECRET_KEY من ملف .env
+SECRET_KEY = os.getenv('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
