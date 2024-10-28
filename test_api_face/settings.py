c450dcf401a5e129e8b4d8d7e42cb2b552ce7a71
AliDalla <alidalla9500@gmail.com>
2024-10-28 23:05:12 +0300

    the 3 commit

--- test_api_face/settings.py
+++ test_api_face/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 from dotenv import load_dotenv
 
 
@@ -82,16 +81,16 @@ WSGI_APPLICATION = 'test_api_face.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-    #DATABASES = {
-   #     'default': {
-      #      'ENGINE': 'django.db.backends.sqlite3',
-      #      'NAME': BASE_DIR / 'db.sqlite3',
-      #  }
-    #}
-    
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+    
+#DATABASES = {
+#    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+#}
 
 
 
